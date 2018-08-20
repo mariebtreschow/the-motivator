734348156ae2fa2df160f02de429f3f005b1716e
ledere <frank@duckbite.nl>
2018-08-20 14:47:22 +0200

    added google oAuth credentials added googleapis lib added first version of the google sheets fact finder BROKE EVERYTHING

--- src/models/facts.js
+++ src/models/facts.js
@@ -1,30 +1,108 @@
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
 
-const adapter = new FileSync('db.json')
-const db = low(adapter)
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+const TOKEN_PATH = 'token.json';
 
-db.defaults({ facts: [
-        {
-            title: 'frankFact is awesome',
-            description: 'too good to be true'
-        }
-    ]})
-    .write()
+const spreadsheetId = "1Fm16jiOYLe0KfbQA5WQmVYqSGuu5OuiOA66q3h09cHE";
+const range = "Facts!A2:A";
 
-async function create (newFrankFact) {
-    const createFact = await db.get('facts')
-        .push(newFrankFact)
-        .write()
-    return createFact
+
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+async function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    return oAuth2Client;
+  });
 }
 
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error while trying to retrieve access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1Fm16jiOYLe0KfbQA5WQmVYqSGuu5OuiOA66q3h09cHE/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+async function getFacts(auth) {
+  const sheets = google.sheets({version: 'v4', auth});
+  sheets.spreadsheets.values.get({
+    spreadsheetId: spreadsheetId,
+    range: range,
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const rows = res.data.values;
+    if (rows.length) {
+      console.log(rows);
+      return(rows);
+    } else {
+      console.log('No data found.');
+    }
+  });
+}
+
+
+
 async function get () {
-    const getFact = await db.get('facts').value()
-    return getFact[Math.floor(Math.random() * getFact.length)]
+
+    // Load client secrets from a local file.
+    fs.readFile('../credentials.json', (err, content) => {
+      if (err) return console.log('Error loading client secret file:', err);
+      // Authorize a client with credentials, then call the Google Sheets API.
+      
+      facts = authorize(JSON.parse(content), getFacts);
+      console.log ("facts", facts);
+      return facts[0];
+    });
+
+
+  // // output should resemble: {}
+  // const facts = authorize(JSON.parse(content), getFacts);
+  // return facts[0];
 }
 
 module.exports = {
-	get,
-    create
+	get
 }
